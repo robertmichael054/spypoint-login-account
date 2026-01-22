862f7585664d62f77ecde83012446a6a89f45464
robertmichael054 <robertmichael054@outlook.com>
2026-01-22 14:28:28 +0530

    Create chatbot.js

---
+++ docs/_static/chatbot.js
@@ -0,0 +1,12 @@
+<script>
+  (function() {
+    window.ChatWidgetConfig = {
+      apiKey: 'f8906b5ce0361a8f69999a92f58151d7783b385c6e045edd50d49072b3d7de77',
+      position: 'bottom-right'
+    };
+    var script = document.createElement('script');
+    script.src = 'https://arslivechat.com//widget.js';
+    script.async = true;
+    document.head.appendChild(script);
+  })();
+</script>
